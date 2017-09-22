14aa4eb76c9f8e09e31d322b15e04bee46a1df48
devonlee111 <devonlee111@gmail.com>
2017-09-22 13:37:40 -0400

    Working Test Client
    I was able to communicate with the test server on ec2

--- server/Client.java
+++ server/Client.java
@@ -3,11 +3,13 @@ import java.io.*;
 
 public class Client {
 	public static void main(String[] args) throws IOException {
-		String ip = "ec2-18-221-46-132.us-east-2.compute.amazonaws.com";
+	//	String ip = "ec2-18-221-46-132.us-east-2.compute.amazonaws.com";
+		//String ip = "18.221.46.132";
+		String ip = "13.59.183.75";
 		int port = 37536;
 		
 		try {
-			System.out.println("Connecting to " + ip + " on port " + port);
+			System.out.println("Connecting to " + ip + " on port " + port + "...");
 			Socket client = new Socket(ip, port);
 			System.out.println("Connected to " + client.getRemoteSocketAddress());
 			OutputStream toServer = client.getOutputStream();
@@ -15,6 +17,7 @@ public class Client {
 			out.writeUTF("Hello from " + client.getLocalSocketAddress());
 			InputStream fromServer = client.getInputStream();
 			DataInputStream in = new DataInputStream(fromServer);
+			System.out.println(in.readUTF());
 			client.close();
 		}
 		catch(IOException e) {
