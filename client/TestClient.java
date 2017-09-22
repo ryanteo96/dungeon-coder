c019bb9efdc60c6c596bcc279f0fa5a8f92c2e66
devonlee111 <devonlee111@gmail.com>
2017-09-22 15:16:30 -0400

    Cleaned Test Server/Client
    Cleaned up the code of the test server and test client a little.

--- client/TestClient.java
+++ client/TestClient.java
@@ -3,20 +3,28 @@ import java.io.*;
 
 public class TestClient {
 	public static void main(String[] args) throws IOException {
-	//	String ip = "ec2-18-221-46-132.us-east-2.compute.amazonaws.com";
-		//String ip = "18.221.46.132";
 		String ip = "13.59.183.75";
 		int port = 37536;
 		
+		// Attempt to connect to the server and send/recieve raw data.	
 		try {
+			// Create a new socket to connect to the server.
 			System.out.println("Connecting to " + ip + " on port " + port + "...");
 			Socket client = new Socket(ip, port);
 			System.out.println("Connected to " + client.getRemoteSocketAddress());
+			
+			// Create an output stream for data going to the server.
 			OutputStream toServer = client.getOutputStream();
 			DataOutputStream out = new DataOutputStream(toServer);
+
+			// Send data to server.
 			out.writeUTF("Hello from " + client.getLocalSocketAddress());
+			
+			// Create an input stream for data from server.
 			InputStream fromServer = client.getInputStream();
 			DataInputStream in = new DataInputStream(fromServer);
+
+			// Read recieved data.
 			System.out.println(in.readUTF());
 			client.close();
 		}
--- server/Server.java
+++ server/Server.java
@@ -3,29 +3,27 @@ import java.io.*;
 
 public class Server {
 	public static void main(String[] args) throws IOException {
-		try {
-			InetAddress ip = InetAddress.getLocalHost();
-			System.out.println("Server ip is : " + ip.getHostAddress());	
-		}
-		catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
-		URL whatismyip = new URL("http://checkip.amazonaws.com");
-		BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
-		String ip2 = in.readLine();
-		System.out.println("What's my ip : " + ip2);
-
 		int port = 37536;
+		// Run forever (currently fails after first connection)
 		while(true) {
 			try {
+				// Wait for client to connect to server
 				System.out.println("Waiting for client on port " + port + "...");
+				
+				// Create new Server Socket for the server.
 				ServerSocket serverSocket = new ServerSocket(port);
+				
+				// Accept incoming connection.
 				Socket server = serverSocket.accept();
 				System.out.println(server.getRemoteSocketAddress() + " just connected.");
+				// Create a new Data Input Stream to recieve data from the client.
 				DataInputStream toServer = new DataInputStream(server.getInputStream());
 			System.out.println(toServer.readUTF());
+
+				// Create a new Data Output Stream to send data to the client.
 				DataOutputStream toClient = new DataOutputStream(server.getOutputStream());
-			toClient.writeUTF("You have connected to " + server.getLocalSocketAddress());
+				// Send data to the client.
+				toClient.writeUTF("You have connected to " + server.getLocalSocketAddress());
 			}
 			catch (SocketTimeoutException s) {
 				System.out.println("Socket timed out!");
