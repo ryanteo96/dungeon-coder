6949b1850b4274508b92e8564b96d4bf08055950
Ryan <ryanteo1996@gmail.com>
2017-09-22 17:55:23 -0400

    Game code setup.

---
+++ game/desktop/src/com/mygdx/dungeoncoder/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.dungeoncoder.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.dungeoncoder.DungeonCoder;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DungeonCoder(), config);
+	}
+}
