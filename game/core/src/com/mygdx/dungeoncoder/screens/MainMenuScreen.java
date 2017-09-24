04212899afd1c72461fd9821f855b9e786688be0
Ryan <ryanteo1996@gmail.com>
2017-09-24 17:12:43 -0400

    Added Main menu and values folder.

---
+++ game/core/src/com/mygdx/dungeoncoder/screens/MainMenuScreen.java
@@ -0,0 +1,80 @@
+package com.mygdx.dungeoncoder.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.mygdx.dungeoncoder.DungeonCoder;
+
+import static com.mygdx.dungeoncoder.values.DefaultValues.VIRTUAL_HEIGHT;
+import static com.mygdx.dungeoncoder.values.DefaultValues.VIRTUAL_WIDTH;
+
+public class MainMenuScreen implements Screen {
+
+    private DungeonCoder game;
+    private Stage stage;
+
+    public MainMenuScreen(DungeonCoder g) {
+        game = g;
+        stage = new Stage(new ScalingViewport(Scaling.fit, VIRTUAL_WIDTH, VIRTUAL_HEIGHT,
+                new OrthographicCamera(VIRTUAL_WIDTH, VIRTUAL_HEIGHT)));
+
+        Gdx.input.setInputProcessor(stage);
+
+        Skin skin = new Skin(Gdx.files.internal("comic-ui.json"));
+
+        Label lblGameLogo = new Label("Instructional", skin, "alt");
+        lblGameLogo.setPosition(360, 500);
+        lblGameLogo.setSize(500,500);
+        lblGameLogo.setFontScale(2);
+
+        stage.addActor(lblGameLogo);
+
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(172/255f, 115/255f, 57/255f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.draw();
+        stage.act(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
