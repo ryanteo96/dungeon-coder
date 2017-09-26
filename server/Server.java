26487b94a144326a27055aa676425cb91caed5b4
Devon Lee <devonlee111@gmail.com>
2017-09-26 20:03:28 +0000

    Connected to Database
    Connected to server to the database in order to test connection and querying from the server.

--- server/Server.java
+++ server/Server.java
@@ -15,22 +15,47 @@ public class Server {
 			System.out.println("Socket timed out!");
 		}
 		
+		//==============================================
+		//SQL DB testing
+		System.out.println("Loading Mysql Driver");	
 		try {
-			// Get driver for mysql
 			Class.forName("com.mysql.jdbc.Driver");
-			// Connect to database
-			Connection con = DriverManager.getConnection("jbdc:mysql://localhost:3306/NAME", "root", "root");
-			Statement stmt = con.createStatement();
-			ResultSet rs=stmt.executeQuery("query");
+			System.out.println("Driver Loaded");
+		}
+		catch (ClassNotFoundException e) { 
+			System.out.println("Driver not found");
+		}
+
+		String url = "jdbc:mysql://dbinstance.ch8dbkdmk4qi.us-east-2.rds.amazonaws.com/userAccounts";
+		String username = "dungeoncoder";
+		String password = "DungeonCoder23";
+	
+		try {
+		// Connect to database
+			Connection conn = DriverManager.getConnection(url, username, password);
+			System.out.println("Connected to Database");
+			Statement stmt = conn.createStatement();
+			
+			System.out.println("Querying for user info");
+			ResultSet rs=stmt.executeQuery("SELECT USERNAME, PASSWORD FROM UserAccounts");
+		
 			while(rs.next()) {
-				
+				System.out.println(rs.getString("USERNAME") + ", " + rs.getString("PASSWORD"));
 			}
 		}
-		catch (Exception e) {
-			System.out.println("failed to connect to database");
-			//e.printStackTrace();
+		catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("SQLException: " + e.getMessage());
+			System.out.println("SQLState: " + e.getSQLState());
+			System.out.println("VendorError: " + e.getErrorCode());
 		}
-		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		//============================================
+
+		System.out.println("\nListening for new connections.");		
 		while(true) {
 			// Accept a new connection and spawn a a new thread to handle it.
 			try {
