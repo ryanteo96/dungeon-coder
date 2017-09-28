df7c23b4d436e0957151ce479b200e52acb9175e
Ryan <ryanteo1996@gmail.com>
2017-09-27 22:50:19 -0400

    Completed CreateAccount html and added new files.

--- web/SignIn.js
+++ web/SignIn.js
@@ -1,3 +1,12 @@
 function body_onload() {
-    
+    signInBtn.onclick = signInBtn_onclick;
+    createAccountBtn.onclick = createAccountBtn_onclick;
+}
+
+function signInBtn_onclick() {
+    alert("In progress.");
+}
+
+function createAccountBtn_onclick() {
+    window.location.href = "CreateAccount.html";
 }
