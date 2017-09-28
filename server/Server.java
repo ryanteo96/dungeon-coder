9e6e87addcf89c69d89421d1acc0280b34892e69
Devon Lee <devonlee111@gmail.com>
2017-09-28 14:02:42 +0000

    Implemented Server Password Hash&Salt and Login
    Implemented functionality on the server to recieve requests for creating and logging into existing accounts. Passwords are stored in the datavase using the SHA512 hash with a 16 byte salt.

--- server/Server.java
+++ server/Server.java
@@ -15,49 +15,10 @@ public class Server {
 			System.out.println("Socket timed out!");
 		}
 		
-		//==============================================
-		//SQL DB testing
-		System.out.println("Loading Mysql Driver");	
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("Driver Loaded");
-		}
-		catch (ClassNotFoundException e) { 
-			System.out.println("Driver not found");
-		}
-
-		String url = "jdbc:mysql://dbinstance.ch8dbkdmk4qi.us-east-2.rds.amazonaws.com/userAccounts";
-		String username = "dungeoncoder";
-		String password = "DungeonCoder23";
-	
-		try {
-		// Connect to database
-			Connection conn = DriverManager.getConnection(url, username, password);
-			System.out.println("Connected to Database");
-			Statement stmt = conn.createStatement();
-			
-			System.out.println("Querying for user info");
-			ResultSet rs=stmt.executeQuery("SELECT USERNAME, PASSWORD FROM UserAccounts");
-		
-			while(rs.next()) {
-				System.out.println(rs.getString("USERNAME") + ", " + rs.getString("PASSWORD"));
-			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("SQLException: " + e.getMessage());
-			System.out.println("SQLState: " + e.getSQLState());
-			System.out.println("VendorError: " + e.getErrorCode());
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		//============================================
-
 		System.out.println("\nListening for new connections.");		
 		while(true) {
 			// Accept a new connection and spawn a a new thread to handle it.
+			System.out.println("listening");
 			try {
 				Socket socket = serverSocket.accept();
 				System.out.println(socket.getRemoteSocketAddress() + " just connected.");
--- server/ServerThread.java
+++ server/ServerThread.java
@@ -1,10 +1,22 @@
+import java.security.*;
+import java.sql.*;
+import javax.sql.rowset.serial.SerialBlob;
 import java.net.*;
 import java.io.*;
 
 public class ServerThread extends Thread {
 	private Socket socket;
-	DataInputStream incoming;
-	DataOutputStream outgoing;
+	private DataInputStream incoming;
+	private DataOutputStream outgoing;
+	
+	private String dburl = "jdbc:mysql://dbinstance.ch8dbkdmk4qi.us-east-2.rds.amazonaws.com/userAccounts";
+	private String dbUsername = "dungeoncoder";
+	private String dbPassword = "DungeonCoder23";
+	private Connection conn = null;
+	private Statement stmt = null;
+	private ResultSet rs = null;
+
+
 	public ServerThread(Socket socket) {
 		this.socket = socket;		
 		try {
@@ -16,7 +28,7 @@ public class ServerThread extends Thread {
 		}
 	}
 
-	public void sendData(String data) {
+	private void sendData(String data) {
 		try {
 			outgoing.writeUTF(data);	
 		}
@@ -27,13 +39,220 @@ public class ServerThread extends Thread {
 		}
 	}
 
+	private void connectDB() {
+		try {
+			conn = DriverManager.getConnection(dburl, dbUsername, dbPassword);
+			stmt = conn.createStatement();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println("SQLException: " + e.getMessage());
+			System.out.println("SQLState: " + e.getSQLState());
+			System.out.println("VendorError: " + e.getErrorCode());	
+		}
+	}
+
+	private void disconnectDB() {
+		if (rs != null) {
+			try {
+				rs.close();
+			}
+			catch(SQLException e) {
+				// Do Nothing
+			}
+		}
+		if (stmt != null) {
+			try {
+				stmt.close();
+			}
+			catch(SQLException e) {
+				// Do Nothing
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch(SQLException e) {
+				// Do Nothing
+			}
+		}
+	}
+
+	private boolean userExists(String username) {
+		if (conn == null) {
+			connectDB();
+		}
+		try {
+			rs = stmt.executeQuery("SELECT USERNAME FROM UserAccounts");
+			while(rs.next()) {
+				if (username.equals(rs.getString("USERNAME"))) {
+					return true;
+				}
+			}
+		}
+		catch(SQLException e) {
+			System.out.println("Something went wrong while fetching users");
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	private String login() {
+		String loginStatus = "username or password is incorrect";
+		String username = "";
+		String password = "";
+		
+		sendData("y");
+
+		try {
+			username = incoming.readUTF();
+			password = incoming.readUTF();
+			System.out.println("Attempting to login with username: " + username + " and password: " + password);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		if (conn == null) {
+			connectDB();
+		}
+		try {
+			rs = stmt.executeQuery("SELECT USERNAME, HASH, SALT FROM UserAccounts");
+			while (rs.next()) {
+				if (username.equals(rs.getString("username"))) {
+					String knownHash = rs.getString("hash");
+					String hexSalt = rs.getString("salt");
+					System.out.println("known salt in hex form is: " + hexSalt);
+					byte[] salt = hexToSalt(hexSalt);
+					System.out.println("known hash is: " + knownHash);
+					System.out.println("tested hash is: " + hash(password, salt));
+					if (knownHash.equals(hash(password, salt))) {
+						loginStatus = "login successful";
+					}
+				}
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return loginStatus;
+	}
+
+	private void createAccount() {
+		sendData("y");
+		String username = "";
+		String password = "";
+		
+		try {
+			username = incoming.readUTF();
+			if (userExists(username)) {
+				outgoing.writeUTF("Username already exists");
+				return;
+			}
+			else {
+				outgoing.writeUTF("Username available");
+			}
+			password = incoming.readUTF();
+			System.out.println("Creating new account with username: " + username + " and password: " + password); 
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		byte[] salt = salt();
+		System.out.println("hashing with salt " + saltyString(salt));
+		String hash = hash(password, salt);
+		System.out.println("Initial hash is" + hash);
+		String hexSalt = saltyString(salt);
+		if (conn == null) {
+			connectDB();
+		}
+		try {
+			stmt.executeUpdate("INSERT INTO UserAccounts (USERNAME, HASH, SALT)" + "VALUES ('" + username + "','" + hash + "','" + hexSalt + "')");
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String saltyString(byte[] salt) {
+		char[] hex = "0123456789ABCDEF".toCharArray();
+		char[] hexChars = new char[salt.length * 2];
+		for (int i = 0; i < salt.length; i++) {
+			int j = salt[i] & 0xFF;
+			hexChars[i * 2] = hex[j >>> 4];
+			hexChars[i * 2 + 1] = hex[j & 0x0f];
+		}
+		return new String(hexChars);
+	}
+
+	private byte[] hexToSalt(String hex) {
+		int length = hex.length();
+		byte[] salt = new byte[length / 2];
+		for (int i = 0; i < length; i += 2) {
+			salt[i / 2] = (byte)((Character.digit(hex.charAt(i), 16) << 4) + Character.digit(hex.charAt(i + 1), 16));
+		}
+		return salt;
+	}
+
 	// To Be Implemented
-	public void sendFile(File file) {
+	private void sendFile(File file) {
 		
 	}
 
-	public void recieveFile() {
+	private void recieveFile() {
+		sendData("y");
+		try {
+			System.out.println("recieving file name...");
+			String fileName = incoming.readUTF();
+			OutputStream out =  new FileOutputStream(fileName);
+			System.out.println("recieving file size...");
+			long fileSize = incoming.readLong();
+			byte[] buffer = new byte[16 * 1024];
+			int count;
+			System.out.println("writing bytes to file...");
+			while (fileSize > 0 && (count = incoming.read(buffer, 0, (int)Math.min(buffer.length, fileSize))) != -1) {
+				out.write(buffer, 0, count);
+				fileSize -= count;
+			}
+			out.close();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 
+	// Generate a new salt value.
+	private byte[] salt() {
+		byte[] salt = new byte[16];
+		SecureRandom random = new SecureRandom();
+		random.nextBytes(salt);
+		return salt;
+	}
+
+	// Hash a password with a given salt.
+	private String hash(String password, byte[] salt) {
+		String hash = "";
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-512");
+			md.update(salt);
+			byte[] bytes = md.digest(password.getBytes("UTF-8"));
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < bytes.length; i++) {
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			hash = sb.toString();
+		}
+		catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return hash;
 	}	
 
 	public void run() {
@@ -62,26 +281,15 @@ public class ServerThread extends Thread {
 				break;
 
 				case "SendFile" :
-					sendData("y");
-					try {
-						System.out.println("recieving file name");
-						String fileName = incoming.readUTF();
-						OutputStream out = new FileOutputStream(fileName);
-						System.out.println("recieving file size");
-						long fileSize = incoming.readLong();
+					recieveFile();
+				break;
 
-						byte[] buffer = new byte[16 * 1024];
-						int count;
-						System.out.println("writing bytes to file");
-						while (fileSize > 0 && (count = incoming.read(buffer, 0, (int)Math.min(buffer.length, fileSize))) != -1) {
-							out.write(buffer, 0, count);
-							fileSize -= count;
-						}
-						out.close();
-					}
-					catch (IOException ex) {
-						ex.printStackTrace();
-					}
+				case "CreateAccount" :
+					createAccount();
+				break;
+
+				case "login" :
+					sendData(login());
 				break;
 
 				default:
