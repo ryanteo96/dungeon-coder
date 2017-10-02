1f41d477aa788a720e12dfde6d86425e8e7bb1d2
lchoo <lchoo1996@gmail.com>
2017-10-02 14:44:03 -0400

    create a stage

---
+++ game/core/src/com/mygdx/dungeoncoder/screens/StageOne.java
@@ -0,0 +1,93 @@
+package com.mygdx.dungeoncoder.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.mygdx.dungeoncoder.DungeonCoder;
+
+import static com.badlogic.gdx.utils.Scaling.fit;
+import static com.mygdx.dungeoncoder.values.DefaultValues.VIRTUAL_HEIGHT;
+import static com.mygdx.dungeoncoder.values.DefaultValues.VIRTUAL_WIDTH;
+
+
+public class StageOne implements Screen {
+    private DungeonCoder game;
+    private Stage stage;
+
+    public StageOne (DungeonCoder g) {
+        game = g;
+        stage = new Stage(new ScalingViewport(Scaling.fit, VIRTUAL_WIDTH, VIRTUAL_HEIGHT,
+                new OrthographicCamera(VIRTUAL_WIDTH, VIRTUAL_HEIGHT)));
+        Gdx.input.setInputProcessor(stage);
+        createBack();
+       }
+
+    private void createBack() {
+        Texture backicon = new Texture(Gdx.files.internal("UIElements/back.png"));
+        TextureRegion backiconRegion = new TextureRegion(backicon);
+        TextureRegionDrawable backiconDrawable = new TextureRegionDrawable(backiconRegion);
+        Image backiconImage = new Image(backiconDrawable);
+        backiconImage.setPosition(0,680);
+        backiconImage.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                backToInstructionalMode(game);
+            }
+        });
+        stage.addActor(backiconImage);
+    }
+
+    private void backToInstructionalMode(DungeonCoder g) {
+        g.setScreen(new InstructionalMode(g));
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(172/255f, 115/255f, 57/255f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.draw();
+        stage.act(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
