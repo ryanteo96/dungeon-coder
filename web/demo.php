3b82c9b0717480b90487e078e3269bdd886a0799
Ryan <ryanteo1996@gmail.com>
2017-10-04 18:57:30 -0400

    test

--- web/demo.php
+++ web/demo.php
@@ -1,5 +1,13 @@
 <?php
-$dsn = 'mysql:host=dbinstance.ch8dbkdmk4qi.us-east-2.rds.amazonaws.com;port=3306;dbname=userAccounts';
+$myObj->name = "John";
+$myObj->age = 30;
+$myObj->city = "New York";
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+
+/*$dsn = 'mysql:host=dbinstance.ch8dbkdmk4qi.us-east-2.rds.amazonaws.com;port=3306;dbname=userAccounts';
 $username = 'dungeoncoder';
 $password = 'DungeonCoder23';
 
@@ -7,5 +15,5 @@ $dbh = new PDO($dsn, $username, $password);
 
 $result = $dbh->query('SELECT USERNAME FROM UserAccounts');
 
-echo $result;
+echo $result;*/
 ?>
