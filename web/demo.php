67ae4f71ad70c5672c3cce25b5f20ca3d7fe8f55
Ryan <ryanteo1996@gmail.com>
2017-10-04 17:18:33 -0400

    bug fixes

--- web/demo.php
+++ web/demo.php
@@ -1,14 +1,11 @@
 <?php
-mysql -h dbinstance.ch8dbkdmk4qi.us-east-2.rds.amazonaws.com -P 3306 -u dungeoncoder -p
-
-
 $dsn = 'mysql:host=dbinstance.ch8dbkdmk4qi.us-east-2.rds.amazonaws.com;port=3306;dbname=userAccounts';
 $username = 'dungeoncoder';
 $password = 'DungeonCoder23';
 
 $dbh = new PDO($dsn, $username, $password);
 
-$result = mysql_query('SELECT USERNAME FROM UserAccounts');
+$result = $dbh->query('SELECT USERNAME FROM UserAccounts');
 
 echo $result;
 ?>
