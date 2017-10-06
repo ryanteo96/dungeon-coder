093e7da6ccf0a8c9177eea8913f760bb8fca621c
Ryan <ryanteo1996@gmail.com>
2017-10-06 00:39:34 -0400

    updated TeacherDashboard

---
+++ web/StudentList.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "dbinstance.ch8dbkdmk4qi.us-east-2.rds.amazonaws.com";
+$username = "dungeoncoder";
+$password = "DungeonCoder23";
+$dbname = "userAccounts";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$param = "java Hash ";
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+$sql = "SELECT Username FROM Users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["Username"]."&";
+    }
+}
+
+$conn->close();
+
+?>
--- web/TeacherDashboard.js
+++ web/TeacherDashboard.js
@@ -1,3 +1,79 @@
+var entries;
+
 function body_onload() {
+    retrieveStudentList();
+}
+
+function displayStudents() {
+    studentListDivData.innerHTML = "";
+    console.log(entries);
+    console.log(entries.length);
+    // traversing the array of entries and creating HTML components and CSS styles.
+    for(var i = 0; i < entries.length; i++) {
+        console.log("yes");
+        console.log(entries);
+        var entry = entries[i];
+    
+        var row = document.createElement("div");
+        var col1 = document.createElement("div");
+        var col2 = document.createElement("div");
+        var col3 = document.createElement("div");
+        var col4 = document.createElement("div");
+        var col5 = document.createElement("div");
+
+        row.className = "divEntriesRow";
+        col1.className = "divEntriesRowCol1";
+        col2.className = "divEntriesRowCol2";
+        col3.className = "divEntriesRowCol3";
+        col4.className = "divEntriesRowCol4";
+        col5.className = "divEntriesRowCol5";
+
+        col1.innerHTML = entry.studentName;
+        col2.innerHTML = entry.studentID;
+        col3.innerHTML = entry.studentEmail;
+        col4.innerHTML = entry.studentScore;
+        col5.innerHTML = entry.studentTag;
         
+        /*row.ondblclick = row_onblclick // run corresponding functions when buttons are pressed.
+        row.index = i; 
+        row.style.cursor = 'pointer'; // changing the pointer when mouse is hovered over.*/
+
+        row.appendChild(col1);
+        row.appendChild(col2);
+        row.appendChild(col3);
+        row.appendChild(col4);
+        row.appendChild(col5);
+
+        studentListDivData.appendChild(row);
+    }
+}
+
+function retrieveStudentList() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var response = this.responseText;
+            var array = response.split("&");
+            parseEntries(array);
+        }
+    };
+    xmlhttp.open("GET", "StudentList.php", true);
+    xmlhttp.send();
+}
+
+function parseEntries(array) {
+    entries = new Array();
+    for (var i = 0; i < array.length - 1; i++) {
+        var entry = new Object();
+        entry.studentName = array[i];
+        entry.studentID = "NOTHING";
+        entry.studentEmail = "NOTHING";
+        entry.studentScore = "NOTHING";
+        entry.studentTag = "NOTHING";
+        console.log(entry);
+        entries.push(entry);
+    }
+    console.log(entries);
+    displayStudents();
 }
