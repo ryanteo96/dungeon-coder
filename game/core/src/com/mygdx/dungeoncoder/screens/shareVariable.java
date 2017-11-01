2dadc148ee62c5050f2514e1c9a28f22a27a3b6c
devonlee111 <devonlee111@gmail.com>
2017-11-01 03:56:32 +0000

    Updated Client Automatic Ping
    Moved the thread that automatically pings the server and made it possible to start the pinging thread from anywhere.

--- game/core/src/com/mygdx/dungeoncoder/screens/shareVariable.java
+++ game/core/src/com/mygdx/dungeoncoder/screens/shareVariable.java
@@ -3,10 +3,25 @@ import com.mygdx.dungeoncoder.utils.ClientConnection;
 
 
 public class shareVariable {
-    public static ClientConnection connect = null;
+    	public static ClientConnection connect = null;
+	public static Pinger ping = null;
 
     public static boolean connected = false;
     public static final int WIDTH = 650;
     public static final int HEIGHT = 500;
     public static final int PPM = 100; //1meter = 100pixels PPM = pixel per meter
+
+	public static class Pinger extends Thread {
+		public Pinger() {
+		}
+
+		public void run() {
+			try {
+				this.sleep(30000);
+			}
+			catch(InterruptedException e) {
+			}
+			shareVariable.connect.ping();
+		}
+	}
 }
