b1ab7316416ef0fa3ac92ab62c1f755ab239be8f
devonlee111 <devonlee111@gmail.com>
2017-11-07 16:48:40 -0500

    Updated EC2 SSH Script

--- aws/ec2.sh
+++ aws/ec2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i "DungeonCoder.pem" ec2-user@ec2-13-59-159-206.us-east-2.compute.amazonaws.com
+ssh -i "DungeonCoder3.pem" ec2-user@ec2-18-216-178-229.us-east-2.compute.amazonaws.com
