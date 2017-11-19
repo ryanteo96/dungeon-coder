b947458d0aca697b77acbe3b9963a389345832a0
devonlee111 <devonlee111@gmail.com>
2017-11-19 12:25:47 -0500

    Implemented Possible Code Runner
    Updated the code evaluator to be able to run a java file.

--- game/core/src/com/mygdx/dungeoncoder/utils/CodeEvaluator.java
+++ game/core/src/com/mygdx/dungeoncoder/utils/CodeEvaluator.java
@@ -17,25 +17,35 @@ public class CodeEvaluator {
 		return compiled;
     	}
 
-		private static void printLines(String name, InputStream ins) throws Exception {
-			String line = null;
-			BufferedReader in = new BufferedReader(new InputStreamReader(ins));
-			while ((line = in.readLine()) != null) {
-				System.out.println(name + " " + line);
-			}
+	private static void printLines(String name, InputStream ins) throws Exception {
+		String line = null;
+		BufferedReader in = new BufferedReader(new InputStreamReader(ins));
+		while ((line = in.readLine()) != null) {
+			System.out.println(name + " " + line);
 		}
+	}
 
       	private static boolean compile(String command) throws Exception {
         	Process pro = Runtime.getRuntime().exec(command);
-			printLines(command + " stdout:", pro.getInputStream());
-			printLines(command + " stderr:", pro.getErrorStream());
+		printLines(command + " stdout:", pro.getInputStream());
+		printLines(command + " stderr:", pro.getErrorStream());
         	pro.waitFor();
 
       		if (pro.exitValue() == 0) {
 			return true;
 		}
-		else {
-			return false;
+		return false;
+	}
+
+	private static boolean run(String filePath) throws Exception {
+		String command = "java " + filePath;
+		Process pro = Runtime.getRuntime().exec(command);
+		printLines(command + " stdout: ", pro.getInputStream());
+		printLines(command + " stderr: ", pro.getErrorStream());
+		pro.waitFor();
+		if (pro.exitValue() == 0) {
+			return true;
 		}
+		return false;
 	}
 }
